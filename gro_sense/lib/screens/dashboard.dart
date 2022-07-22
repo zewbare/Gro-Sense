0b900f3b9aaf4eec6b44552e026f36b548bc2aea
NiraliAmrutiya <niraliamrutiya1996@gmail.com>
2022-07-22 17:28:21 -0400

    Dashboard minor changes

--- gro_sense/lib/screens/dashboard.dart
+++ gro_sense/lib/screens/dashboard.dart
@@ -1,10 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-import '../widget.dart';
-
-class FetchData {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+import '../Model/user_model.dart';
 
 class DashBoardScreen extends StatefulWidget {
   const DashBoardScreen({Key? key}) : super(key: key);
@@ -14,126 +12,283 @@ class DashBoardScreen extends StatefulWidget {
 }
 
 class _DashBoardScreenState extends State<DashBoardScreen> {
-  // final _auth = FirebaseAuth.instance;
+  final _auth = FirebaseAuth.instance;
+  UserModel loggedInUser = UserModel();
+
+  List<Map<String, dynamic>> userMap = [];
+  bool isLoading = false;
+  List productData = [];
+  int? userId;
+
+  Future getData() async {
+    setState(() {
+      isLoading = true;
+    });
+    User? currentUser = FirebaseAuth.instance.currentUser;
+    FirebaseFirestore.instance.collection('users').get().then((value) {
+      //  print(value.size);
+      // for(int i=0;i<value.size;i++){
+      //
+      // }
+      // print(userMap);
+      for (int i = 0; i < value.size; i++) {
+        userMap.add(value.docs[i].data());
+        if(currentUser?.uid==(userMap[i]['uid'])){
+          userId=i;
+          print("--------------userIndex---------"+userId.toString());
+        }
+      }
+
+      if(userMap[userId!]['Add Product']!=null){
+        for (int i = 0; i < userMap[userId!]['Add Product'].length; i++) {
+          productData.add(userMap[userId!]['Add Product'][i]);
+        }
+        print("product data------------------"+productData.length.toString());
+      }
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
 
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: SafeArea(
-        child: Container(
+
+      body: SafeArea (
+        child: isLoading
+            ? const Center(
+          child: Center(
+            child: CircularProgressIndicator(
+              valueColor:
+              AlwaysStoppedAnimation<Color>(Colors.blueAccent),
+            ),
+          ),
+        )
+            : Container(
           width: double.infinity,
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          color: Color(0xFFF6F6F6),
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          color: const Color(0xFFF6F6F6),
           child: Stack(children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 5.0,
-                  ),
-                  margin: EdgeInsets.only(top: 25.0, bottom: 10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Welcome",
-                        style: TextStyle(
-                          color: Color(0x89000000),
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                          top: 5.0,
-                        ),
-                        child: Text(
-                          "Nirali Amrutiya", // To check
+            SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 5.0,
+                    ),
+                    margin: const EdgeInsets.only(top: 25.0, bottom: 10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget> [
+                        Text(
+                          "Welcome",
                           style: TextStyle(
-                            fontSize: 22.0,
-                            height: 1.0,
                             color: Color(0xFF000000),
-                            fontWeight: FontWeight.bold,
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w800,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                    margin: EdgeInsets.only(top: 2.0, bottom: 10.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image(
-                        image: AssetImage("assets/banner1.png"),
-                        width: double.infinity,
-                      ),
-                    )),
-                Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.symmetric(
-                    // horizontal: 24.0,
-                    vertical: 10.0,
-                  ),
-                  margin: EdgeInsets.only(bottom: 10.0),
-                  decoration: BoxDecoration(
-                    // color: Colors.white,
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Upcoming Expires",
-                        style: TextStyle(
-                          color: Color(0x9E000000),
-                          fontSize: 20.0,
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 5.0,
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                          top: 5.0,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                    child: ListView(
-                  children: [
-                    TaskCardWidget(
-                      // priority: "High",
-                      title: "Cookies",
-                      description: "Expires in 2 days",
+                  Container(
+                      margin: EdgeInsets.only(top: 2.0, bottom: 10.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image(
+                          image: AssetImage("assets/banner1.png"),
+                          width: double.infinity,
+                        ),
+                      )),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                      // horizontal: 24.0,
+                      vertical: 10.0,
                     ),
-                    TaskCardWidget(
-                      // priority: "Low",
-                      title: "Bread",
-                      description: "Expires in 3 days",
+                    margin: EdgeInsets.only(bottom: 10.0),
+                    decoration: BoxDecoration(
+                      // color: Colors.white,
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    TaskCardWidget(
-                      // priority: "Low",
-                      title: "Milk",
-                      description: "Expires in 5 days",
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Upcoming Expires",
+                          style: TextStyle(
+                            color: Color(0x9E000000),
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 5.0,
+                          ),
+                        ),
+                      ],
                     ),
-                    TaskCardWidget(
-                      // priority: "Medium",
-                      title: "Spinach",
-                      description: "Expires in 5 days",
+                  ),
+
+                  userMap[0]['Add Product']!=null?
+                  Container(
+                    height: 600,
+                    child: ListView.builder(
+                      physics: BouncingScrollPhysics(),
+                      itemBuilder: (BuildContext context, int index) {
+
+                        return buildItem(context,productData[index]);
+                      },
+                      itemCount: productData.length,
+                      // controller: listScrollController,
                     ),
-                  ],
-                ))
-              ],
+                  ):Container(
+                    height: 200,
+                    child: Center(child: Text("No data found")),
+                  ),
+
+                ],
+              ),
             ),
           ]),
         ),
       ),
+      // floatingActionButton: FloatingActionButton.extended(
+      //   elevation: 5.0,
+      //   icon: const Icon(Icons.add),
+      //   label: const Text('Add'),
+      //   onPressed: () {
+      //     Navigator.of(context).pushReplacement(
+      //         MaterialPageRoute(builder: (context) => AddaProductScreen()));
+      //   },
+      // ),
+      // floatingActionButtonLocation:
+      // FloatingActionButtonLocation.centerDocked,
+      // bottomNavigationBar: BottomAppBar(
+      //
+      //   // hasNotch: false,
+      //   child: new Row(
+      //     mainAxisSize: MainAxisSize.max,
+      //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //     children: <Widget>[
+      //       // IconButton(
+      //       //   icon: Icon(Icons.home),
+      //       //   onPressed: () {},
+      //       // ),
+      //       // IconButton(
+      //       //   icon: Icon(Icons.menu),
+      //       //   onPressed: () {
+      //       //     Navigator.of(context).pushReplacement(
+      //       //     MaterialPageRoute(builder: (context) => tabbarPage()));
+      //       //
+      //       //   },
+      //       // ),
+      //       // SizedBox(width:20),
+      //       // IconButton(
+      //       //   icon: Icon(Icons.card_giftcard_rounded),
+      //       //
+      //       //   onPressed: () {
+      //       //     Navigator.of(context).pushReplacement(
+      //       //         MaterialPageRoute(builder: (context) => LocationScreen()));
+      //       //   },
+      //       // ),
+      //       // IconButton(
+      //       //   icon: Icon(Icons.settings),
+      //       //   onPressed: () {
+      //       //     Navigator.of(context).pushReplacement(
+      //       //         MaterialPageRoute(builder: (context) => SettingsScreen()));
+      //       //   },
+      //       // )
+      //     ],
+      //   ),
+      // ),
+    );
+  }
+}
+
+DateTime formatTimestamp(Timestamp timestamp) {
+  var format = new DateFormat('y-MM-d'); // <- use skeleton here
+  return timestamp.toDate();
+}
+Widget buildItem(BuildContext context,map) {
+  print("-------------------------->>>>>"+map.toString());
+  print(map);
+  if (map != null) {
+
+    var date = new DateTime.now();
+
+    final difference =formatTimestamp(map['expiryDate']).difference(date).inDays;
+    return difference <0?Container(
+      height: MediaQuery.of(context).size.height,
+      width:MediaQuery.of(context).size.width,
+      child: Text("No expired products"),
+    ) :Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(
+        horizontal: 24.0,
+        vertical: 26.0,
+      ),
+      margin: EdgeInsets.only(bottom: 10.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          Text(
+            map['productName'].toString(),
+            style: TextStyle(
+              color: Color(0xFF000000),
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 5.0,
+            ),
+            child: Text(
+              formatTimestamp(map['expiryDate']).toString().substring(0,10),
+              style: TextStyle(
+                fontSize: 16.0,
+                height: 1.0,
+                color: Color(0xFFA8A8A8),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+  } else {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width:MediaQuery.of(context).size.width,
+      child: Text("No Data Found"),
     );
   }
 }
